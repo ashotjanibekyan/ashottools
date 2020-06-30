ac8585f0955b97c80dd10a0207ad04d1a4ace8bd
Ashot Janibekyan <ashotjanibekyan@gmail.com>
2020-06-30 08:14:19 +0400

    իմաստ չկա ամբողջը հաշվել

--- wikielections/queries/edits.sql
+++ wikielections/queries/edits.sql
@@ -1,7 +1,7 @@
-SELECT COUNT(*) edits
-FROM revision
+SELECT COUNT(*)
+FROM (SELECT 1 FROM revision
 JOIN actor ON rev_actor = actor_id
 WHERE actor_name = 'USERNAME'
     AND rev_timestamp < STARTDATE
     AND rev_timestamp > ENDDATE
-LIMIT 1000
+LIMIT 1000) edits
